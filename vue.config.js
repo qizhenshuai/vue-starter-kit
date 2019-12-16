849afa46bb37c1012b6adbf67a54d73a22434ab2
qishuai <qishuai@huizhaofang.com>
2019-12-16 13:47:13 +0800

    update sass-loader

--- vue.config.js
+++ vue.config.js
@@ -56,7 +56,7 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://127.0.0.1:${port}/mock`,
+        target: process.env.VUE_APP_PROXY_API,
         secure: false,
         changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
         // ws: true, // 是否启用websockets
